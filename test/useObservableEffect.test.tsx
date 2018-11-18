d9512838b3a3275e74e0fb6aa21f785d81035f3b
Javier Gonzalez <xaviergonz@users.noreply.github.com>
2018-11-18 20:14:01 +0100

    Use observable effect disposer test (#26)

--- test/useObservableEffect.test.tsx
+++ test/useObservableEffect.test.tsx
@@ -1,14 +1,15 @@
-import { observable, reaction } from "mobx"
+import { IReactionDisposer, observable, reaction } from "mobx"
 import * as React from "react"
 import { cleanup, render } from "react-testing-library"
 import { observer, useObservableEffect } from "../src"
 
 afterEach(cleanup)
 
-test("useObservableEffect", async () => {
+test("reactions run and dispose properly", async () => {
     let reactions1 = 0
     let reactions2 = 0
     let renders = 0
+    let reactionDisposerCalls = 0
 
     const store = observable({
         prop1: 0,
@@ -16,46 +17,84 @@ test("useObservableEffect", async () => {
     })
 
     const Component = observer((props: { store: typeof store }) => {
-        useObservableEffect(() =>
-            reaction(
+        useObservableEffect(() => {
+            const disposer = reaction(
                 () => props.store.prop1,
                 () => {
                     reactions1++
                 }
             )
-        )
 
-        useObservableEffect(() =>
-            reaction(
+            return (() => {
+                reactionDisposerCalls++
+                disposer()
+            }) as IReactionDisposer
+        })
+
+        useObservableEffect(() => {
+            const disposer = reaction(
                 () => props.store.prop2,
                 () => {
                     reactions2++
                 }
             )
-        )
+
+            return (() => {
+                reactionDisposerCalls++
+                disposer()
+            }) as IReactionDisposer
+        })
 
         renders++
         return (
             <div>
-                `${props.store.prop1} ${props.store.prop2}`
+                {props.store.prop1} {props.store.prop2}
             </div>
         )
     })
 
     const { rerender } = render(<Component store={store} />)
+    expect(reactionDisposerCalls).toBe(0)
     expect(renders).toBe(1)
     expect(reactions1).toBe(0)
     expect(reactions2).toBe(0)
 
     store.prop1 = 1
     rerender(<Component store={store} />)
+    expect(reactionDisposerCalls).toBe(0)
     expect(renders).toBe(2)
     expect(reactions1).toBe(1)
     expect(reactions2).toBe(0)
 
     store.prop2 = 1
     rerender(<Component store={store} />)
+    expect(reactionDisposerCalls).toBe(0)
     expect(renders).toBe(3)
     expect(reactions1).toBe(1)
     expect(reactions2).toBe(1)
+
+    rerender(<div />)
+    expect(reactionDisposerCalls).toBe(2)
+    expect(renders).toBe(3)
+    expect(reactions1).toBe(1)
+    expect(reactions2).toBe(1)
+})
+
+test("an undefined disposer works", async () => {
+    let renders = 0
+
+    const Component = observer(() => {
+        useObservableEffect(() => {
+            return undefined as any
+        })
+
+        renders++
+        return <div>test</div>
+    })
+
+    const { rerender } = render(<Component />)
+    expect(renders).toBe(1)
+
+    rerender(<div />)
+    expect(renders).toBe(1)
 })
