963673d64a5245d5dd449dfadcb43e1fac7d174e
Daniel K <fredy.c@seznam.cz>
2018-11-18 15:24:50 +0100

    Fix name of package in assert environment

--- src/assertEnvironment.ts
+++ src/assertEnvironment.ts
@@ -2,8 +2,8 @@ import { spy } from "mobx"
 import { useState } from "react"
 
 if (!useState) {
-    throw new Error("mobx-react requires React 16.7 to be available")
+    throw new Error("mobx-react-lite requires React 16.7 to be available")
 }
 if (!spy) {
-    throw new Error("mobx-react requires mobx to be available")
+    throw new Error("mobx-react-lite requires mobx to be available")
 }
