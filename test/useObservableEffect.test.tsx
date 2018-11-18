942612b1d6d8eae4fec954661b9bdb757bb506da
Javier Gonzalez <xaviergonz@gmail.com>
2018-11-18 18:37:32 +0100

    add test for disposer being called

--- test/useObservableEffect.test.tsx
+++ test/useObservableEffect.test.tsx
@@ -1,4 +1,4 @@
-import { observable, reaction } from "mobx"
+import { IReactionDisposer, observable, reaction } from "mobx"
 import * as React from "react"
 import { cleanup, render } from "react-testing-library"
 import { observer, useObservableEffect } from "../src"
@@ -9,6 +9,7 @@ test("useObservableEffect", async () => {
     let reactions1 = 0
     let reactions2 = 0
     let renders = 0
+    let reaction2DisposerCalls = 0
 
     const store = observable({
         prop1: 0,
@@ -25,14 +26,19 @@ test("useObservableEffect", async () => {
             )
         )
 
-        useObservableEffect(() =>
-            reaction(
+        useObservableEffect(() => {
+            const disposer = reaction(
                 () => props.store.prop2,
                 () => {
                     reactions2++
                 }
             )
-        )
+
+            return (() => {
+                reaction2DisposerCalls++
+                disposer()
+            }) as IReactionDisposer
+        })
 
         renders++
         return (
@@ -43,18 +49,27 @@ test("useObservableEffect", async () => {
     })
 
     const { rerender } = render(<Component store={store} />)
+    expect(reaction2DisposerCalls).toBe(0)
     expect(renders).toBe(1)
     expect(reactions1).toBe(0)
     expect(reactions2).toBe(0)
 
     store.prop1 = 1
     rerender(<Component store={store} />)
+    expect(reaction2DisposerCalls).toBe(0)
     expect(renders).toBe(2)
     expect(reactions1).toBe(1)
     expect(reactions2).toBe(0)
 
     store.prop2 = 1
     rerender(<Component store={store} />)
+    expect(reaction2DisposerCalls).toBe(0)
+    expect(renders).toBe(3)
+    expect(reactions1).toBe(1)
+    expect(reactions2).toBe(1)
+
+    rerender(<div />)
+    expect(reaction2DisposerCalls).toBe(1)
     expect(renders).toBe(3)
     expect(reactions1).toBe(1)
     expect(reactions2).toBe(1)
