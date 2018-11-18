08f6bf71b57c7d542e9856f9383c50b6b904203f
Javier Gonzalez <xaviergonz@gmail.com>
2018-11-18 01:07:10 +0100

    ref support for observer components

--- src/observer.ts
+++ src/observer.ts
@@ -1,5 +1,5 @@
 import { Reaction } from "mobx"
-import { FunctionComponent, memo, useCallback, useRef, useState } from "react"
+import { forwardRef, memo, useCallback, useRef, useState } from "react"
 import { useUnmount } from "./utils"
 
 let isUsingStaticRendering = false
@@ -8,10 +8,14 @@ export function useStaticRendering(enable: boolean) {
     isUsingStaticRendering = enable
 }
 
-export function observer<P>(baseComponent: FunctionComponent<P>): FunctionComponent<P> {
+export function observer<P extends object, TRef = {}>(
+    baseComponent: React.RefForwardingComponent<TRef, P>
+): React.MemoExoticComponent<
+    React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<TRef>>
+> {
     // The working of observer is explaind step by step in this talk: https://www.youtube.com/watch?v=cPF4iBedoF0&feature=youtu.be&t=1307
     if (isUsingStaticRendering) {
-        return baseComponent
+        return baseComponent as any
     }
 
     const baseComponentName = baseComponent.displayName || baseComponent.name
@@ -19,20 +23,26 @@ export function observer<P>(baseComponent: FunctionComponent<P>): FunctionCompon
     // memo; we are not intested in deep updates
     // in props; we assume that if deep objects are changed,
     // this is in observables, which would have been tracked anyway
-    const memoComponent = memo((props: P) => {
-        const observerReaction = useObserverReaction(baseComponentName)
+    const memoComponent = memo(
+        // we have to use forwardRef here because:
+        // 1. it cannot go before memo, only after it
+        // 2. forwardRef converts the function into an actual component, so we can't let the baseComponent do it
+        //    since it wouldn't be a callable function anymore
+        forwardRef((props: P, ref: any) => {
+            const observerReaction = useObserverReaction(baseComponentName)
 
-        // render the original component, but have the
-        // reaction track the observables, so that rendering
-        // can be invalidated (see above) once a dependency changes
-        let rendering!: ReturnType<typeof baseComponent>
-        observerReaction.track(() => {
-            rendering = baseComponent(props)
+            // render the original component, but have the
+            // reaction track the observables, so that rendering
+            // can be invalidated (see above) once a dependency changes
+            let rendering!: ReturnType<typeof baseComponent>
+            observerReaction.track(() => {
+                rendering = baseComponent(props, ref)
+            })
+            return rendering
         })
-        return rendering
-    })
+    )
     memoComponent.displayName = baseComponentName
-    return memoComponent as any
+    return memoComponent
 }
 
 function useForceUpdate() {
