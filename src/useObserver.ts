754c6eb0e79e526699304ae03c72dab01ad9b62c
Michel Weststrate <mweststrate@gmail.com>
2019-02-14 14:47:54 +0100

    Introduced printing debug value to show hooks state

--- src/useObserver.ts
+++ src/useObserver.ts
@@ -1,5 +1,5 @@
-import { Reaction } from "mobx"
-import { useRef } from "react"
+import { Reaction, getDependencyTree, IDependencyTree } from "mobx"
+import { useRef, useDebugValue } from "react"
 import { isUsingStaticRendering } from "./staticRendering"
 import { useForceUpdate, useUnmount } from "./utils"
 
@@ -7,7 +7,6 @@ export function useObserver<T>(fn: () => T, baseComponentName = "observed"): T {
     if (isUsingStaticRendering()) {
         return fn()
     }
-
     const forceUpdate = useForceUpdate()
 
     const reaction = useRef<Reaction | null>(null)
@@ -16,6 +15,8 @@ export function useObserver<T>(fn: () => T, baseComponentName = "observed"): T {
             forceUpdate()
         })
     }
+
+    useDebugValue(reaction, printDebugValue)
 
     useUnmount(() => {
         reaction.current!.dispose()
@@ -29,4 +30,23 @@ export function useObserver<T>(fn: () => T, baseComponentName = "observed"): T {
         rendering = fn()
     })
     return rendering
+}
+
+function printDebugValue(v: React.MutableRefObject<Reaction | null>) {
+    if (!v.current) return "<unknown>"
+    // not the nicest representation, and we would rather have an expandable object
+    // hopefully that is added later to the devtools..
+    const lines: string[] = []
+    printDepTree(getDependencyTree(v.current), lines, 0)
+    return lines.join("   ")
+}
+
+// from observable.ts in MobX
+function printDepTree(tree: IDependencyTree, lines: string[], depth: number) {
+    if (lines.length >= 1000) {
+        lines.push("(and many more)")
+        return
+    }
+    lines.push(`${new Array(depth).join("\t")}${tree.name}`)
+    if (tree.dependencies) tree.dependencies.forEach(child => printDepTree(child, lines, depth + 1))
 }
