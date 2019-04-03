44ee8c203c84cc8f36fdefc10ec72c2fecb82bd5
Royston Shufflebotham <Royston.Shufflebotham@uk.ibm.com>
2019-04-03 12:56:29 +0100

    Test cleaning up reactions for uncommitted components

--- test/useObserver.test.tsx
+++ test/useObserver.test.tsx
@@ -4,6 +4,7 @@ import * as React from "react"
 import { act, cleanup, render } from "react-testing-library"
 
 import { useObserver } from "../src"
+import { resetCleanupScheduleForTests } from "../src/useObserver"
 
 afterEach(cleanup)
 
@@ -39,4 +40,44 @@ test("uncommitted observing components should not attempt state changes", () => 
     } finally {
         restoreConsole()
     }
+})
+
+test("uncommitted components should not leak observations", async () => {
+    resetCleanupScheduleForTests()
+
+    jest.useFakeTimers()
+
+    const store = mobx.observable({ count1: 0, count2: 0 })
+
+    // Track whether counts are observed
+    let count1IsObserved = false
+    let count2IsObserved = false
+    mobx.onBecomeObserved(store, "count1", () => (count1IsObserved = true))
+    mobx.onBecomeUnobserved(store, "count1", () => (count1IsObserved = false))
+    mobx.onBecomeObserved(store, "count2", () => (count2IsObserved = true))
+    mobx.onBecomeUnobserved(store, "count2", () => (count2IsObserved = false))
+
+    const TestComponent1 = () => useObserver(() => <div>{store.count1}</div>)
+    const TestComponent2 = () => useObserver(() => <div>{store.count2}</div>)
+
+    // Render, then remove only #2
+    const rendering = render(
+        <React.StrictMode>
+            <TestComponent1 />
+            <TestComponent2 />
+        </React.StrictMode>
+    )
+    rendering.rerender(
+        <React.StrictMode>
+            <TestComponent1 />
+        </React.StrictMode>
+    )
+
+    // Allow any reaction-disposal cleanup timers to run
+    jest.runAllTimers()
+
+    // count1 should still be being observed by Component1,
+    // but count2 should have had its reaction cleaned up.
+    expect(count1IsObserved).toBeTruthy()
+    expect(count2IsObserved).toBeFalsy()
 })
