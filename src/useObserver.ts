6e797edc1b047f30a242ee063335799b7df22920
Royston Shufflebotham <Royston.Shufflebotham@uk.ibm.com>
2019-04-03 12:56:53 +0100

    First attempt at a fix for cleaning up reactions from uncommitted components

--- src/useObserver.ts
+++ src/useObserver.ts
@@ -36,11 +36,13 @@ export function useObserver<T>(
                 forceUpdate()
             }
         })
+        scheduleCleanupOfReactionIfNotCommitted(reaction.current)
     }
 
     useDebugValue(reaction, printDebugValue)
 
     useEffect(() => {
+        recordReactionAsCommitted(reaction.current!)
         committed.current = true
         return () => reaction.current!.dispose()
     }, [])
@@ -62,4 +64,54 @@ export function useObserver<T>(
         throw exception // re-throw any exceptions catched during rendering
     }
     return rendering
+}
+
+/**
+ * Reactions created by components that have yet to be committed.
+ */
+const uncommittedReactions: Reaction[] = []
+
+/**
+ * Latest 'uncommitted reactions' cleanup timer handle
+ */
+let reactionCleanupHandle: number | undefined
+
+function scheduleCleanupOfReactionIfNotCommitted(reaction: Reaction) {
+    uncommittedReactions.push(reaction)
+    if (!reactionCleanupHandle) {
+        // We currently have no cleanup timer running; schedule one
+        reactionCleanupHandle = window.setTimeout(cleanUncommittedReactions, 100)
+    }
+}
+
+function recordReactionAsCommitted(reaction: Reaction) {
+    // It would be more efficient if we could use a Set instead of an Array,
+    // but mobx-react-lite currently supports MobX 4 and ES5, so we can't assume
+    // the presence of Set.
+    const i = uncommittedReactions.indexOf(reaction)
+    if (i) {
+        uncommittedReactions.splice(i, 1)
+    }
+}
+
+/**
+ * Run by the cleanup timer to dispose any outstanding reactions
+ */
+function cleanUncommittedReactions() {
+    reactionCleanupHandle = undefined
+
+    while (uncommittedReactions.length) {
+        uncommittedReactions.pop()!.dispose()
+    }
+}
+
+/**
+ * Only to be used by test functions; do not export outside of mobx-react-lite
+ */
+export function resetCleanupScheduleForTests() {
+    if (reactionCleanupHandle) {
+        clearTimeout(reactionCleanupHandle)
+        reactionCleanupHandle = undefined
+    }
+    uncommittedReactions.length = 0
 }
