ce23dbbc634f008570f5b7acf65db63125f4b93c
John Reilly <johnny_reilly@hotmail.com>
2019-04-14 09:51:21 +0100

    Fix typo in comment of observer.ts

--- src/observer.ts
+++ src/observer.ts
@@ -21,7 +21,7 @@ export function observer<P extends object, TRef = {}>(
     baseComponent: React.RefForwardingComponent<TRef, P>,
     options?: IObserverOptions
 ) {
-    // The working of observer is explaind step by step in this talk: https://www.youtube.com/watch?v=cPF4iBedoF0&feature=youtu.be&t=1307
+    // The working of observer is explained step by step in this talk: https://www.youtube.com/watch?v=cPF4iBedoF0&feature=youtu.be&t=1307
     if (isUsingStaticRendering()) {
         return baseComponent
     }
