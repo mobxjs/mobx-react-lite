cc7985c1f5e867841e46cee1754a9ce158c848c8
Michel Weststrate <mweststrate@gmail.com>
2019-04-17 15:12:33 +0200

    Added tests counting re-renders

--- test/useAsObservableSource.test.tsx
+++ test/useAsObservableSource.test.tsx
@@ -3,72 +3,196 @@ import * as React from "react"
 import { useEffect, useState } from "react"
 import { act, cleanup, render } from "react-testing-library"
 
-import { observer, useAsObservableSource, useLocalStore, useObserver } from "../src"
+import { observer, Observer, useAsObservableSource, useLocalStore, useObserver } from "../src"
 
 afterEach(cleanup)
 
-test("base useAsObservableSource should work", () => {
-    let counterRender = 0
-    let observerRender = 0
+describe("base useAsObservableSource should work", () => {
+    it("with useObservable", () => {
+        let counterRender = 0
+        let observerRender = 0
 
-    function Counter({ multiplier }: { multiplier: number }) {
-        counterRender++
-        const observableProps = useAsObservableSource({ multiplier })
-        const store = useLocalStore(() => ({
-            count: 10,
-            get multiplied() {
-                return observableProps.multiplier * this.count
-            },
-            inc() {
-                this.count += 1
-            }
-        }))
+        function Counter({ multiplier }: { multiplier: number }) {
+            counterRender++
+            const observableProps = useAsObservableSource({ multiplier })
+            const store = useLocalStore(() => ({
+                count: 10,
+                get multiplied() {
+                    return observableProps.multiplier * this.count
+                },
+                inc() {
+                    this.count += 1
+                }
+            }))
 
-        return useObserver(
-            () => (
-                observerRender++,
-                (
-                    <div>
-                        Multiplied count: <span>{store.multiplied}</span>
-                        <button id="inc" onClick={store.inc}>
-                            Increment
-                        </button>
-                    </div>
+            return useObserver(
+                () => (
+                    observerRender++,
+                    (
+                        <div>
+                            Multiplied count: <span>{store.multiplied}</span>
+                            <button id="inc" onClick={store.inc}>
+                                Increment
+                            </button>
+                        </div>
+                    )
                 )
             )
-        )
-    }
+        }
 
-    function Parent() {
-        const [multiplier, setMultiplier] = useState(1)
+        function Parent() {
+            const [multiplier, setMultiplier] = useState(1)
 
-        return (
-            <div>
-                <Counter multiplier={multiplier} />
-                <button id="incmultiplier" onClick={() => setMultiplier(m => m + 1)} />
-            </div>
-        )
-    }
+            return (
+                <div>
+                    <Counter multiplier={multiplier} />
+                    <button id="incmultiplier" onClick={() => setMultiplier(m => m + 1)} />
+                </div>
+            )
+        }
 
-    const { container } = render(<Parent />)
+        const { container } = render(<Parent />)
 
-    expect(container.querySelector("span")!.innerHTML).toBe("10")
-    expect(counterRender).toBe(1)
-    expect(observerRender).toBe(1)
+        expect(container.querySelector("span")!.innerHTML).toBe("10")
+        expect(counterRender).toBe(1)
+        expect(observerRender).toBe(1)
 
-    act(() => {
-        ;(container.querySelector("#inc")! as any).click()
+        act(() => {
+            ;(container.querySelector("#inc")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("11")
+        expect(counterRender).toBe(2) // 1 would be better!
+        expect(observerRender).toBe(2)
+
+        act(() => {
+            ;(container.querySelector("#incmultiplier")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("22")
+        expect(counterRender).toBe(4) // TODO: avoid double rendering here!
+        expect(observerRender).toBe(4) // TODO: avoid double rendering here!
     })
-    expect(container.querySelector("span")!.innerHTML).toBe("11")
-    expect(counterRender).toBe(2)
-    expect(observerRender).toBe(2)
 
-    act(() => {
-        ;(container.querySelector("#incmultiplier")! as any).click()
+    it("with <Observer>", () => {
+        let counterRender = 0
+        let observerRender = 0
+
+        function Counter({ multiplier }: { multiplier: number }) {
+            counterRender++
+            const observableProps = useAsObservableSource({ multiplier })
+            const store = useLocalStore(() => ({
+                count: 10,
+                get multiplied() {
+                    return observableProps.multiplier * this.count
+                },
+                inc() {
+                    this.count += 1
+                }
+            }))
+
+            return (
+                <Observer>
+                    {() => {
+                        observerRender++
+                        return (
+                            <div>
+                                Multiplied count: <span>{store.multiplied}</span>
+                                <button id="inc" onClick={store.inc}>
+                                    Increment
+                                </button>
+                            </div>
+                        )
+                    }}
+                </Observer>
+            )
+        }
+
+        function Parent() {
+            const [multiplier, setMultiplier] = useState(1)
+
+            return (
+                <div>
+                    <Counter multiplier={multiplier} />
+                    <button id="incmultiplier" onClick={() => setMultiplier(m => m + 1)} />
+                </div>
+            )
+        }
+
+        const { container } = render(<Parent />)
+
+        expect(container.querySelector("span")!.innerHTML).toBe("10")
+        expect(counterRender).toBe(1)
+        expect(observerRender).toBe(1)
+
+        act(() => {
+            ;(container.querySelector("#inc")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("11")
+        expect(counterRender).toBe(1)
+        expect(observerRender).toBe(2)
+
+        act(() => {
+            ;(container.querySelector("#incmultiplier")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("22")
+        expect(counterRender).toBe(2)
+        expect(observerRender).toBe(3)
     })
-    expect(container.querySelector("span")!.innerHTML).toBe("22")
-    expect(counterRender).toBe(4) // TODO: avoid double rendering here!
-    expect(observerRender).toBe(4) // TODO: avoid double rendering here!
+
+    it("with Observer()", () => {
+        let counterRender = 0
+
+        const Counter = observer(function Counter({ multiplier }: { multiplier: number }) {
+            counterRender++
+
+            const observableProps = useAsObservableSource({ multiplier })
+            const store = useLocalStore(() => ({
+                count: 10,
+                get multiplied() {
+                    return observableProps.multiplier * this.count
+                },
+                inc() {
+                    this.count += 1
+                }
+            }))
+
+            return (
+                <div>
+                    Multiplied count: <span>{store.multiplied}</span>
+                    <button id="inc" onClick={store.inc}>
+                        Increment
+                    </button>
+                </div>
+            )
+        })
+
+        function Parent() {
+            const [multiplier, setMultiplier] = useState(1)
+
+            return (
+                <div>
+                    <Counter multiplier={multiplier} />
+                    <button id="incmultiplier" onClick={() => setMultiplier(m => m + 1)} />
+                </div>
+            )
+        }
+
+        const { container } = render(<Parent />)
+
+        expect(container.querySelector("span")!.innerHTML).toBe("10")
+        expect(counterRender).toBe(1)
+
+        act(() => {
+            ;(container.querySelector("#inc")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("11")
+        expect(counterRender).toBe(2)
+
+        act(() => {
+            ;(container.querySelector("#incmultiplier")! as any).click()
+        })
+        expect(container.querySelector("span")!.innerHTML).toBe("22")
+        expect(counterRender).toBe(4) // TODO: should be 3
+    })
 })
 
 test("useAsObservableSource with effects should work", () => {
