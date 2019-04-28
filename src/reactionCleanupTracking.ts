62c8620837f6ab3e084f828b6a798ce3e6c97aab
Royston Shufflebotham <royston@shufflebotham.org>
2019-04-28 10:59:54 +0100

    Schedule uncommitted reaction cleanup (#121)
    * Test cleaning up reactions for uncommitted components
    
    * First attempt at a fix for cleaning up reactions from uncommitted components
    
    * Use debounce instead of fixed interval
    
    * Add unit test for missing observable changes before useEffect runs
    
    This is a test to check that observable changes made between the
    first component render and commit are not lost.
    
    It currently fails (and did so before the change in PR #119)
    
    * Add test for cleanup timer firing too early for some components
    
    This demonstrates (in a slightly contrived way) how, if the
    cleanup timer fires between a recent component being
    rendered and it being committed, that it would incorrectly
    tidy up a reaction for a soon-to-be-committed component.
    
    * Update test for missing changes to check Strict and non-Strict mode
    
    We had an existing test to check that observable changes between render
    and commit didn't go missing, but it only checked in Strict mode, and
    there's a problem with non-Strict mode.
    
    * Add cleanup tracking and more tests
    
    This adds full cleanup tracking, and even more tests:
    
    - we now track how long ago potentially leaked reactions
    were created, and only clean those that were leaked 'a while ago'
    - if a reaction is incorrectly disposed because a component
    went away for a very long time and came back again later
    (in a way React doesn't even do right now), we safely recreate it and re-render
    - trap the situation where a change is made to a tracked observable
    between first render and commit (where we couldn't force an update
    because we hadn't _been_ committed) and force a re-render
    - more unit tests
    
    * Fix renamed test file
    
    When I renamed this file, I forgot the .test. suffix. D'oh.
    
    * Extract tracking and cleanup logic out to separate file
    
    * Update src/useObserver.ts
    
    Co-Authored-By: RoystonS <royston@shufflebotham.org>
    
    * Move some more tracking internals into the tracking code

---
+++ src/reactionCleanupTracking.ts
@@ -0,0 +1,125 @@
+import { Reaction } from "mobx"
+
+export interface IReactionTracking {
+    /** The Reaction created during first render, which may be leaked */
+    reaction: Reaction
+    /**
+     * The time (in ticks) at which point we should dispose of the reaction
+     * if this component hasn't yet been fully mounted.
+     */
+    cleanAt: number
+
+    /**
+     * Whether the component has yet completed mounting (for us, whether
+     * its useEffect has run)
+     */
+    mounted?: boolean
+
+    /**
+     * Whether the observables that the component is tracking changed between
+     * the first render and the first useEffect.
+     */
+    changedBeforeMount?: boolean
+}
+
+export function createTrackingData(reaction: Reaction) {
+    const trackingData: IReactionTracking = {
+        cleanAt: Date.now() + CLEANUP_LEAKED_REACTIONS_AFTER_MILLIS,
+        reaction
+    }
+    return trackingData
+}
+
+/**
+ * The minimum time before we'll clean up a Reaction created in a render
+ * for a component that hasn't managed to run its effects. This needs to
+ * be big enough to ensure that a component won't turn up and have its
+ * effects run without being re-rendered.
+ */
+export const CLEANUP_LEAKED_REACTIONS_AFTER_MILLIS = 10_000
+
+/**
+ * The frequency with which we'll check for leaked reactions.
+ */
+export const CLEANUP_TIMER_LOOP_MILLIS = 10_000
+
+/**
+ * Reactions created by components that have yet to be fully mounted.
+ */
+const uncommittedReactionRefs: Set<React.MutableRefObject<IReactionTracking | null>> = new Set()
+
+/**
+ * Latest 'uncommitted reactions' cleanup timer handle.
+ */
+let reactionCleanupHandle: ReturnType<typeof setTimeout> | undefined
+
+function ensureCleanupTimerRunning() {
+    if (reactionCleanupHandle === undefined) {
+        reactionCleanupHandle = setTimeout(cleanUncommittedReactions, CLEANUP_TIMER_LOOP_MILLIS)
+    }
+}
+
+export function scheduleCleanupOfReactionIfLeaked(
+    ref: React.MutableRefObject<IReactionTracking | null>
+) {
+    uncommittedReactionRefs.add(ref)
+
+    ensureCleanupTimerRunning()
+}
+
+export function recordReactionAsCommitted(
+    reactionRef: React.MutableRefObject<IReactionTracking | null>
+) {
+    uncommittedReactionRefs.delete(reactionRef)
+}
+
+/**
+ * Run by the cleanup timer to dispose any outstanding reactions
+ */
+function cleanUncommittedReactions() {
+    reactionCleanupHandle = undefined
+
+    // Loop through all the candidate leaked reactions; those older
+    // than CLEANUP_LEAKED_REACTIONS_AFTER_MILLIS get tidied.
+
+    const now = Date.now()
+    for (const ref of uncommittedReactionRefs) {
+        const tracking = ref.current
+        if (tracking) {
+            if (now >= tracking.cleanAt) {
+                // It's time to tidy up this leaked reaction.
+                tracking.reaction.dispose()
+                ref.current = null
+                uncommittedReactionRefs.delete(ref)
+            }
+        }
+    }
+
+    if (uncommittedReactionRefs.size > 0) {
+        // We've just finished a round of cleanups but there are still
+        // some leak candidates outstanding.
+        ensureCleanupTimerRunning()
+    }
+}
+
+/* istanbul ignore next */
+/**
+ * Only to be used by test functions; do not export outside of mobx-react-lite
+ */
+export function forceCleanupTimerToRunNowForTests() {
+    // This allows us to control the execution of the cleanup timer
+    // to force it to run at awkward times in unit tests.
+    if (reactionCleanupHandle) {
+        clearTimeout(reactionCleanupHandle)
+        cleanUncommittedReactions()
+    }
+}
+
+/* istanbul ignore next */
+export function resetCleanupScheduleForTests() {
+    if (reactionCleanupHandle) {
+        clearTimeout(reactionCleanupHandle)
+        reactionCleanupHandle = undefined
+    }
+    uncommittedReactionRefs.clear()
+}
--- src/useObserver.ts
+++ src/useObserver.ts
@@ -1,6 +1,12 @@
 import { Reaction } from "mobx"
 import { useDebugValue, useEffect, useRef } from "react"
 import { printDebugValue } from "./printDebugValue"
+import {
+    createTrackingData,
+    IReactionTracking,
+    recordReactionAsCommitted,
+    scheduleCleanupOfReactionIfLeaked
+} from "./reactionCleanupTracking"
 import { isUsingStaticRendering } from "./staticRendering"
 import { useForceUpdate } from "./utils"
 
@@ -11,6 +17,10 @@ export interface IUseObserverOptions {
 }
 
 const EMPTY_OBJECT = {}
+
+function observerComponentNameFor(baseComponentName: string) {
+    return `observer${baseComponentName}`
+}
 
 export function useObserver<T>(
     fn: () => T,
@@ -24,25 +34,69 @@ export function useObserver<T>(
     const wantedForceUpdateHook = options.useForceUpdate || useForceUpdate
     const forceUpdate = wantedForceUpdateHook()
 
-    const reaction = useRef<Reaction | null>(null)
-    const committed = useRef(false)
+    // StrictMode/ConcurrentMode/Suspense may mean that our component is
+    // rendered and abandoned multiple times, so we need to track leaked
+    // Reactions.
+    const reactionTrackingRef = useRef<IReactionTracking | null>(null)
 
-    if (!reaction.current) {
-        // First render for this component. Not yet committed.
-        reaction.current = new Reaction(`observer(${baseComponentName})`, () => {
-            // Observable has changed. Only force an update if we've definitely
-            // been committed.
-            if (committed.current) {
+    if (!reactionTrackingRef.current) {
+        // First render for this component (or first time since a previous
+        // reaction from an abandoned render was disposed).
+
+        const newReaction = new Reaction(observerComponentNameFor(baseComponentName), () => {
+            // Observable has changed, meaning we want to re-render
+            // BUT if we're a component that hasn't yet got to the useEffect()
+            // stage, we might be a component that _started_ to render, but
+            // got dropped, and we don't want to make state changes then.
+            // (It triggers warnings in StrictMode, for a start.)
+            if (trackingData.mounted) {
+                // We have reached useEffect(), so we're mounted, and can trigger an update
                 forceUpdate()
+            } else {
+                // We haven't yet reached useEffect(), so we'll need to trigger a re-render
+                // when (and if) useEffect() arrives.  The easiest way to do that is just to
+                // drop our current reaction and allow useEffect() to recreate it.
+                newReaction.dispose()
+                reactionTrackingRef.current = null
             }
         })
+
+        const trackingData = createTrackingData(newReaction)
+        reactionTrackingRef.current = trackingData
+        scheduleCleanupOfReactionIfLeaked(reactionTrackingRef)
     }
 
+    const { reaction } = reactionTrackingRef.current!
     useDebugValue(reaction, printDebugValue)
 
     useEffect(() => {
-        committed.current = true
-        return () => reaction.current!.dispose()
+        // Called on first mount only
+        recordReactionAsCommitted(reactionTrackingRef)
+
+        if (reactionTrackingRef.current) {
+            // Great. We've already got our reaction from our render;
+            // all we need to do is to record that it's now mounted,
+            // to allow future observable changes to trigger re-renders
+            reactionTrackingRef.current.mounted = true
+        } else {
+            // The reaction we set up in our render has been disposed.
+            // This is either due to bad timings of renderings, e.g. our
+            // component was paused for a _very_ long time, and our
+            // reaction got cleaned up, or we got a observable change
+            // between render and useEffect
+
+            // Re-create the reaction
+            reactionTrackingRef.current = {
+                reaction: new Reaction(observerComponentNameFor(baseComponentName), () => {
+                    // We've definitely already been mounted at this point
+                    forceUpdate()
+                }),
+                cleanAt: Infinity
+            }
+            forceUpdate()
+        }
+
+        return () => reactionTrackingRef.current!.reaction.dispose()
     }, [])
 
     // render the original component, but have the
@@ -50,7 +104,7 @@ export function useObserver<T>(
     // can be invalidated (see above) once a dependency changes
     let rendering!: T
     let exception
-    reaction.current.track(() => {
+    reaction.track(() => {
         try {
             rendering = fn()
         } catch (e) {
@@ -58,7 +112,6 @@ export function useObserver<T>(
         }
     })
     if (exception) {
-        reaction.current.dispose()
         throw exception // re-throw any exceptions catched during rendering
     }
     return rendering
