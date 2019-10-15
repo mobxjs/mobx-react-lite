6efdd0d78b7a8745a81f5d644b14e64950cd66ea
Daniel K <fredy.c@seznam.cz>
2019-10-15 04:59:15 +0200

    Copy types instead of importing

--- src/optimizeScheduler.ts
+++ src/optimizeScheduler.ts
@@ -1,7 +1,12 @@
 import { configure } from "mobx"
-import { unstable_batchedUpdates } from "react-dom"
 
-export const optimizeScheduler = (reactionScheduler: typeof unstable_batchedUpdates) => {
+interface TBatchedUpdated {
+    batchedUpdates<A, B>(callback: (a: A, b: B) => any, a: A, b: B): void
+    batchedUpdates<A>(callback: (a: A) => any, a: A): void
+    batchedUpdates(callback: () => any): void
+}
+
+export const optimizeScheduler = (reactionScheduler: TBatchedUpdated) => {
     if (typeof reactionScheduler === "function") {
         configure({ reactionScheduler })
     }
