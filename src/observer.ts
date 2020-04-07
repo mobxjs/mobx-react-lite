3d297defccc46c2e30e85cc8e2b5ba8bb3d0eb0d
Vladimir <vzh.inbox@gmail.com>
2020-04-07 17:53:36 +0600

    Revert

--- src/observer.ts
+++ src/observer.ts
@@ -7,6 +7,18 @@ export interface IObserverOptions {
     readonly forwardRef?: boolean
 }
 
+export function observer<P extends object, TRef = {}>(
+    baseComponent: React.RefForwardingComponent<TRef, P>,
+    options: IObserverOptions & { forwardRef: true }
+): React.MemoExoticComponent<
+    React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<TRef>>
+>
+
+export function observer<P extends object>(
+    baseComponent: React.FunctionComponent<P>,
+    options?: IObserverOptions
+): React.FunctionComponent<P>
+
 export function observer<
     C extends React.FunctionComponent<any> | React.RefForwardingComponent<any>,
     Options extends IObserverOptions
@@ -15,13 +27,14 @@ export function observer<
     options?: Options
 ): Options extends { forwardRef: true }
     ? C extends React.RefForwardingComponent<infer TRef, infer P>
-        ? React.MemoExoticComponent<
-              React.ForwardRefExoticComponent<
-                  React.PropsWithoutRef<P> & React.RefAttributes<TRef>
+        ? C &
+              React.MemoExoticComponent<
+                  React.ForwardRefExoticComponent<
+                      React.PropsWithoutRef<P> & React.RefAttributes<TRef>
+                  >
               >
-          >
         : never /* forwardRef set for a non forwarding component */
-    : React.MemoExoticComponent<C>;
+    : C;
 
 // n.b. base case is not used for actual typings or exported in the typing files
 export function observer<P extends object, TRef = {}>(
