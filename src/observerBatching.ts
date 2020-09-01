109107e599a5fff8e963b642135ca15e8c3c549c
Veniamin Krol <153412+vkrol@users.noreply.github.com>
2020-09-01 14:15:35 +0300

    Fix typo in observerBatching.ts (defaulNoopBatch -> defaultNoopBatch) (#311)

--- src/observerBatching.ts
+++ src/observerBatching.ts
@@ -1,12 +1,12 @@
 import { configure } from "mobx"
 
-export function defaulNoopBatch(callback: () => void) {
+export function defaultNoopBatch(callback: () => void) {
     callback()
 }
 
 export function observerBatching(reactionScheduler: any) {
     if (!reactionScheduler) {
-        reactionScheduler = defaulNoopBatch
+        reactionScheduler = defaultNoopBatch
         if ("production" !== process.env.NODE_ENV) {
             console.warn(
                 "[MobX] Failed to get unstable_batched updates from react-dom / react-native"
