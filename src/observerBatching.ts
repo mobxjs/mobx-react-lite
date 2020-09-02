b1ffe51ea92e84ed66527e5e23ccf2bdb6e9e72a
Alexander Wilden <alexander.wilden@timeflex.de>
2020-09-02 09:45:10 +0200

    fix: reintroduce isObserverBatched for backwards compatibility (#309)

--- src/observerBatching.ts
+++ src/observerBatching.ts
@@ -1,4 +1,7 @@
 import { configure } from "mobx"
+import { getGlobal, getSymbol } from "./utils"
+
+const observerBatchingConfiguredSymbol = getSymbol("observerBatching")
 
 export function defaultNoopBatch(callback: () => void) {
     callback()
@@ -14,4 +17,7 @@ export function observerBatching(reactionScheduler: any) {
         }
     }
     configure({ reactionScheduler })
+    getGlobal()[observerBatchingConfiguredSymbol] = true
 }
+
+export const isObserverBatched = () => !!getGlobal()[observerBatchingConfiguredSymbol]
