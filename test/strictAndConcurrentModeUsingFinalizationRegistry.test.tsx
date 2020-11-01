8082e92b86b01998350d6ca7b317e2704d0eed13
Bnaya Peretz <me@bnaya.net>
2020-11-01 16:32:03 +0200

    CR

--- test/strictAndConcurrentModeUsingFinalizationRegistry.test.tsx
+++ test/strictAndConcurrentModeUsingFinalizationRegistry.test.tsx
@@ -6,11 +6,14 @@ import { useObserver } from "../src/useObserver"
 import { sleep } from "./utils"
 import { FinalizationRegistry } from "../src/utils/FinalizationRegistryWrapper"
 
+// @ts-ignore
+import gc from "expose-gc/function"
+
 afterEach(cleanup)
 
 test("uncommitted components should not leak observations", async () => {
-    if (!global.gc || !FinalizationRegistry) {
-        throw new Error("This test must run with node >= 14 and --expose-gc flag")
+    if (!FinalizationRegistry) {
+        throw new Error("This test must run with node >= 14")
     }
 
     const store = mobx.observable({ count1: 0, count2: 0 })
@@ -40,12 +43,8 @@ test("uncommitted components should not leak observations", async () => {
     )
 
     // Allow gc to kick in in case to let finalization registry cleanup
-    if (global.gc) {
-        global.gc()
-        await sleep(50)
-    } else {
-        throw new Error("gc must be exposed")
-    }
+    gc()
+    await sleep(50)
 
     // count1 should still be being observed by Component1,
     // but count2 should have had its reaction cleaned up.
